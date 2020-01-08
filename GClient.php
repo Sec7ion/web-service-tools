05b42854ab36d19b8d79e890b207edea75d035e7
Sec7ion <davidwijaya491@gmail.com>
2020-01-09 00:23:30 +0700

    Merapikan syntax

--- GClient.php
+++ GClient.php
@@ -1,35 +1,37 @@
 <?php
-require_once "vendor/autoload.php";
+require "vendor/autoload.php";
+use \GuzzleHttp\Client;
 
 class GClient {
-	private $_Client, $_Result;
+	private $_client, $_result;
 	
 	public function __construct() {
-		$this->_Client = new GuzzleHttp\Client([
+		$this->_client = new Client([
 			"base_uri" => "https://cse.google.com/cse/element/"
 		]);
 	}
-	public function getContents() {
-		return $this->_Result;
+	final public function result() {
+		return $this->_result;
 	}
-	public function gSearch($Jumlah = null, $Dork) {
-		$Token = file_get_contents("https://cse.google.com/cse.js?cx=partner-pub-2698861478625135:3033704849");
-		@preg_match_all('("cse_token": "(.*?)")i', $Token, $API);
-		$Resp = $this->_Client->request("GET", "v1", [
+	final public function search($mulai = 0, $jumlah = 5, $query = "Cara tidur") {
+		$token = file_get_contents("https://cse.google.com/cse.js?cx=partner-pub-2698861478625135:3033704849");
+		@preg_match_all('("cse_token": "(.*?)")i', $token, $API);
+
+		$resp = $this->_client->request("GET", "v1", [
 		"headers"  => [
 				"User-Agent"  => "Mozilla/70.0.1 (Linux; Android 8.1.0; M5) AppleWebKit/604.1 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Version/13.0 Mobile/15E148 Safari/604.1"
 			],
 		"query" => [
-			"hl"					 => "en",
-			"cx"					 => "partner-pub-2698861478625135:3033704849",
-			"safe"			 => "off",
-			"cse_tok"	 => $API[1][0],
-			"start"			 => rand(1, 100),
+			"hl" => "en",
+			"cx" => "partner-pub-2698861478625135:3033704849",
+			"safe" => "off",
+			"cse_tok" => $API[1][0],
+			"start" => $mulai,
 			"callback" => "x",
-			"num"				 => $Jumlah,
-			"q"					 => $Dork
+			"num" => $jumlah,
+			"q" => $query
 			]
 		]);
-		$this->_Result = $Resp->getBody()->getContents();
+		$this->_result = $resp->getBody()->getContents();
 	}
 }
